2b39f25adcfc4f007832d72cdd6eeb4ec6253515
‘Tim <timothy.j.ocampo@gmail.com>
2017-11-15 15:11:12 -0800

    Section 1 Lesson 16: Event Listeners : What did the user type into the search bar?
    				We need to:
    				A.) Create an Event Handler
    				B.) Pass Event handler to the element we want to monitor for events
    
    				ex.) when the user inputs data into search form - ie, when search form is not blank
    
    ex.) ########################## search-bar.js ###############################################
    
    import React, { Component} from 'react';
    
    class SearchBar extends Component {
        render() {
            // return <input />;     <==previous return prior to setting up event handler.
    
    				// C.) TRANSLATION:
    					the line below states: " create a new input element and, pass it as a prop (ie property) on change,
    					with a value of this.onInputChange (the user input/change of the state's property)"
    
            return <input onChange={this.onInputChange} />;
    				//onChange is a reserved var in react delegated for watching changes made to some element.
        }
    
        				// below is our event handler:
        				// A.) whenever the input changes, run the code in here
        				// B.) Pass it to element we want to monitor [ in this case, its the <input> ]
    				//notice we pass a variable called event into the event handler below:
        onInputChange(event){
            console.log(event.target.value);
    				//we can access javascript functions such as .target.value on the variable input “event”
    				so that we can console log and check if event handler works correctly.
        }
    };
    
    export default SearchBar;
    
     ############################################################################################
    
    <NEXT, we’ll clean this code up>

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,17 +1,24 @@
 import React, { Component} from 'react';
 
-// see last push to reference changes made:
-// in this push, converted previous functional component into an ES6 CLASS COMP
-
-
-// 1.) the line bellow can be read as: define a new class as SearchBar, give it access to all functionality that React.Component class inherently posses.
-// 2.) We will also need to define a method within the class in order to render the return of the function. (Defined as: render() {}
 class SearchBar extends Component {
     render() {
-        return <input />;
+        // return <input />;     <==previous return prior to setting up event handler.
+        //
+        return <input onChange={this.onInputChange} />; //onChange is a reserved word delegated for watching changes
+    }   // C.) TRANSLATION: the line above states: " create a new input element and, pass it as a prop (ie property) on change, with a value of this.onInputChange (the user input/change of the state's property)"
+
+    //below is our event handler:
+    // A.) whenever the input changes, run the code in here
+    // B.) Pass it to element we want to monitor [ in this case, its the <input> ]
+
+
+    //notice we pass a variable called event into the event handler below
+    onInputChange(event){
+        console.log(event);
+
     }
 };
 
-//to render the searchBar (child comp, index.js needs a REFERENCE to searchBar.js
 export default SearchBar;
+
 
