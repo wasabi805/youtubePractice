dbbc83d164d68b0f1b1d76df9d03abc041da78b9
‘Tim <timothy.j.ocampo@gmail.com>
2017-11-15 15:51:27 -0800

    //  CREATING EVENT HANDLERS
    import React, { Component} from 'react';
    
    class SearchBar extends Component {
        render() {
            //return <input />;     <==previous return prior to setting up event handler.
            //return <input onChange={this.onInputChange} />;   <==previous return used to define prop change on state of event handler.
    
                                //cleaned up code above used to return change on search-bar when user types data in search form.
            return <input onChange={
                (event) => console.log(event.target.value)
            } />;
        }
    
                                // CLEANING UP THE CODE
                                // Previously, we passed a variable called event into the event handler function below.
    
        // onInputChange(event){
        //     console.log(event.target.value);
        //
        // }
    
                                //Above, we can remove the event handler because of how we restructured the search-bar comp to define the event (onChange for input)
                                //as a function ( by using the => ) that takes var event and  returns a console.log containing the text input by the user within the search-bar (event.target.value).
    
    };
    
    export default SearchBar;

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,22 +1,29 @@
+//  CREATING EVENT HANDLERS
+
 import React, { Component} from 'react';
 
 class SearchBar extends Component {
     render() {
-        // return <input />;     <==previous return prior to setting up event handler.
-        //
-        return <input onChange={this.onInputChange} />; //onChange is a reserved word delegated for watching changes
-    }   // C.) TRANSLATION: the line above states: " create a new input element and, pass it as a prop (ie property) on change, with a value of this.onInputChange (the user input/change of the state's property)"
+        //return <input />;     <==previous return prior to setting up event handler.
+        //return <input onChange={this.onInputChange} />;   <==previous return used to define prop change on state of event handler.
 
-    //below is our event handler:
-    // A.) whenever the input changes, run the code in here
-    // B.) Pass it to element we want to monitor [ in this case, its the <input> ]
-
-
-    //notice we pass a variable called event into the event handler below
-    onInputChange(event){
-        console.log(event);
-
+                            //cleaned up code above used to return change on search-bar when user types data in search form.
+        return <input onChange={
+            (event) => console.log(event.target.value)
+        } />;
     }
+
+                            // CLEANING UP THE CODE
+                            // Previously, we passed a variable called event into the event handler function below.
+
+    // onInputChange(event){
+    //     console.log(event.target.value);
+    //
+    // }
+
+                            //Above, we can remove the event handler because of how we restructured the search-bar comp to define the event (onChange for input)
+                            //as a function ( by using the => ) that takes var event and  returns a console.log containing the text input by the user within the search-bar (event.target.value).
+
 };
 
 export default SearchBar;
