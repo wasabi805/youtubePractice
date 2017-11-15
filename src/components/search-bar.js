79d4cac63c679a7d1d6e210b661b02f62e178ef4
‘Tim <timothy.j.ocampo@gmail.com>
2017-11-14 22:51:47 -0800

    refer to :https://www.udemy.com/react-redux/learn/v4/t/lecture/4288056?start=0 Section 1 Lesson 14: A.) EXPORTING MODULES B.) EXPORTING Classes C.) EXPORTING STATE
    KEY NOTES:
    1.) One page per component:
    created search-bar.js
    created video-detail.js
    created video-list.js
    created video-list-item.js
    
    2.) create the API key in google console developers
    3.) npm install seachbar functionality with:
    	npm install --save youtube-api-search
    
     #############    search-bar.js  ###############
    import React from 'react';
    
    const SearchBar = () => {
        return <input />;
    };
    
    //to render the searchBar (child comp, index.js needs a REFERENCE to searchBar.js
    
    export default SearchBar;
    
     --declared SearchBar as a comp on this page
     --exported that comp
    
     <END>
    
     ###############    index.js ######################
    
     import SearchBar from './components/search-bar';
     const API_KEY = 'AIzaSyB3lwz55zUHvylyF80AEV6hhYBDSLZj9xo'
    
    const App = () => {
        return (
            <div>
                Youtube components
                <SearchBar />
            </div>
        );
    }
    
    ReactDOM.render(<App />, document.querySelector('.root'));
    
     -- finally to render comp, included <SearchBar />in return div.
     <END>

---
+++ src/components/search-bar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return <input />;
+};
+
+//to render the searchBar (child comp, index.js needs a REFERENCE to searchBar.js
+
+export default SearchBar;
+
