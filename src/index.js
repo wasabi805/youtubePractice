4573fdede925f5cc6578f1d65c60e133561570fe
‘Tim <timothy.j.ocampo@gmail.com>
2017-11-16 01:44:10 -0800

    //Section 2 Lesson 01: ##### Youtube Search Response - DOWNWARD DATA FLOW - API - CALLBACKS ##### // //recognition to Stephen Grider: https://www.udemy.com/react-redux/learn/v4/t/lecture/4288070?start=0 // // // ###########      THE FOCUS    ########### // //      I. ) Make API Call to youtube //           -when we get the response back, we'll need to distribute that info through the App //            within relevance to other components within the app. // //      II.) Determine which component should handle the video search //           -currently within our app, to some degree, search-bar.js, video-detail.js, video-list.js, and video-list-item.js //            all need to be informed of the API response in order for themto perform their functions. //                 ...so... //           How do we determine who's job it is to make the API request, interpret the response, and notify the other components? //              -we'll use DOWNWARD DATA FLOW model. // //                -<END FOCUS>- // // // // ###########      DOWNWARD DATA FLOW      ########### // //       DEFINITION: "Only the most parent component in an App should be responsible for fetching data." //          - "In our app, index.js is most parent comp we have" //             children of App are:  search-bar.js, video-detail.js, video-list.js, and video-list-item.js // //               -<END DOWNWARD DATA FLOW>- // // // // // ###########      FETCHING DATA -API - CALLBACK FUNCTION   ########### // //        1.) In order to get access to that SEARCH PACKAGE from Section 01 (npm install --save youtube-api-search), //          we need to IMPORT the SEARCH PACKAGE into index.js // //          ex.) import YTSearch from 'youtube-api-search'; // //        2.) How the youtube-api-search package works: // //              A.) We call the API //              B.) The first arg be an OBJECT with: //                      I. ) an API key //                      II.) SEARCH TERM //             -From what we described what this package will do, it behaves similarly to a function. // //        3.) Example of what search call will look like: // //              ex.)    YTSearch({key: API_KEY, term: 'cats'}, function(data){ //                          console.log(data) //                      }); // //                 A.) YTSearch({})                                <=== YTSearch get passed in an obj //                 B.) YTSearch({key: API_KEY })                     <=== Pass in API_KEY //                 C.) YTSearch({key: API_KEY, term: 'cats' })         <=== Pass in a term with a value //                 D.) function(data){                                   <=== Pass in a function as a second arg, //                          console.log(data);                                this function get passed a var of data, //                      });                                                   we'll console.log data to what the api call returned. // //         4.)  After you've added the youtube search package to index.js, // //                YTSearch({key: API_KEY, term: 'cats'}, function(data){ //                      console.log(data) //                }); // //              go to the browser and check the console: Arrays of objects will appear with API response of JSON objs. //
    //THE PARENT COMPONENT - index.js
    
    import React from 'react';
    import ReactDOM from 'react-dom';
    import YTSearch from 'youtube-api-search';
    
    //COMPONENTS
    import SearchBar from './components/search-bar';
    
    //VARIABLES
    const API_KEY = 'AIzaSyB3lwz55zUHvylyF80AEV6hhYBDSLZj9xo';
    
    YTSearch(
        {key: API_KEY, term: 'cats'},
        function (data) {
            console.log(data)
        });
    
    //Rendered Components go here:
    const App = () => {
        return (
            <div>
                Youtube components
                <SearchBar />
            </div>
        );
    };
    
    ReactDOM.render(<App />, document.querySelector('.root'));

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
+//Section 2 Lesson 01: ##### Youtube Search Response - DOWNWARD DATA FLOW - API - CALLBACKS #####
+
 //THE PARENT COMPONENT - index.js
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 
 //COMPONENTS
 import SearchBar from './components/search-bar';
 
 //VARIABLES
-const API_KEY = 'AIzaSyB3lwz55zUHvylyF80AEV6hhYBDSLZj9xo'
+const API_KEY = 'AIzaSyB3lwz55zUHvylyF80AEV6hhYBDSLZj9xo';
+
+YTSearch(
+    {key: API_KEY, term: 'cats'},
+    function (data) {
+        console.log(data)
+    });
 
 
 //Rendered Components go here:
@@ -21,3 +31,7 @@ const App = () => {
 };
 
 ReactDOM.render(<App />, document.querySelector('.root'));
+
+
+
+
